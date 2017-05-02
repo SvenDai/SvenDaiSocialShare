e66228c65aa60fa15c4627dd246ed7b13de7459b
fdai <fdai@ctrip.com>
2017-05-02 13:28:55 +0800

    Initial Commit

---
+++ SvenDaiSocialShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SvenDaiSocialShare
+//
+//  Created by daifeng on 2017/5/2.
+//  Copyright © 2017年 daifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SvenDaiSocialShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SvenDaiSocialShare
+//
+//  Created by daifeng on 2017/5/2.
+//  Copyright © 2017年 daifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
